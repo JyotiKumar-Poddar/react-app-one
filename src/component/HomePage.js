f2e66637d60e722f949cb61a7bdc30772edbc72e
JyotiKumarPoddar <jyoti@skenariolabs.com>
2020-02-29 18:45:33 +0200

    test commit from vs code

---
+++ src/component/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jabmotron">
+      <h1>PS course</h1>
+      <p> React, flux</p>
+    </div>
+  );
+}
+export default HomePage;
